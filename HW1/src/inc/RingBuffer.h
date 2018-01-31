346c70fe2bf31a4198f15e31b4418340edeb8243
henrywann <henrybryantwang@gmail.com>
2018-01-31 14:06:44 -0500

    Change of Offset function

--- HW1/src/inc/RingBuffer.h
+++ HW1/src/inc/RingBuffer.h
@@ -6,58 +6,58 @@
 #include <cmath>
 
 /*! \brief implement a circular buffer of type T
- */
-template <class T>
+*/
+template <class T> 
 class CRingBuffer
 {
 public:
     explicit CRingBuffer (int iBufferLengthInSamples) :
-    m_iBuffLength(iBufferLengthInSamples),
-    m_iReadIdx(0),
-    m_iWriteIdx(0),
-    m_ptBuff(0)
+        m_iBuffLength(iBufferLengthInSamples),
+        m_iReadIdx(0),                          
+        m_iWriteIdx(0),
+        m_ptBuff(0)
     {
         assert(iBufferLengthInSamples > 0);
-        
+
         m_ptBuff        = new T [m_iBuffLength];
         reset();
     }
-    
+
     virtual ~CRingBuffer ()
     {
         delete [] m_ptBuff;
         m_ptBuff    = 0;
     }
-    
+
     /*! add a new value of type T to write index
-     \param tNewValue the new value
-     \return void
-     */
+    \param tNewValue the new value
+    \return void
+    */
     void put(T tNewValue)
     {
         m_ptBuff[m_iWriteIdx] = tNewValue;
     }
     
     /*! return the value at the current read index
-     \return type T the value from the read index
-     */
+    \return type T the value from the read index
+    */
     T get () const
     {
         return m_ptBuff[m_iReadIdx];
     }
-    
+
     /*! return the current index for writing/put
-     \return int
-     */
+    \return int
+    */
     int getWriteIdx () const
     {
         return m_iWriteIdx;
     }
-    
+
     /*! move the write index to a new position
-     \param iNewWriteIdx: new position
-     \return void
-     */
+    \param iNewWriteIdx: new position
+    \return void
+    */
     void setWriteIdx (int iNewWriteIdx)
     {
         try {
@@ -67,22 +67,22 @@ public:
             std::cout << msg << std::endl;
             return;
         }
-        
+
         m_iWriteIdx = iNewWriteIdx;
     }
-    
+
     /*! return the current index for reading/get
-     \return int
-     */
+    \return int
+    */
     int getReadIdx () const
     {
         return m_iReadIdx;
     }
-    
+
     /*! move the read index to a new position
-     \param iNewReadIdx: new position
-     \return void
-     */
+    \param iNewReadIdx: new position
+    \return void
+    */
     void setReadIdx (int iNewReadIdx)
     {
         try {
@@ -94,11 +94,11 @@ public:
         }
         m_iReadIdx = iNewReadIdx;
     }
-    
+
     /*! add a new value of type T to write index and increment write index
-     \param tNewValue the new value
-     \return void
-     */
+    \param tNewValue the new value
+    \return void
+    */
     void putPostInc (T tNewValue)
     {
         // to be implemented
@@ -114,10 +114,10 @@ public:
             std::cout << "WARNING: you are going to override the value that you have not read" << std::endl;
         }
     }
-    
+
     /*! return the value at the current read index and increment the read pointer
-     \return type T the value from the read index
-     */
+    \return type T the value from the read index
+    */
     T getPostInc ()
     {
         // to be implemented
@@ -131,51 +131,46 @@ public:
         if (m_iWriteIdx == m_iReadIdx && status == 1) {
             status = 0;
             std::cout << "WARNING: the value you are going "
-            "to read is either the value you have already read or a value not defined" << std::endl;
+                    "to read is either the value you have already read or a value not defined" << std::endl;
         }
         return temp;
     }
-    
+
     /*! return the value at the index with an arbitrary offset
-     \param iOffset: read at offset from read index
-     \return type T the value from the read index
-     */
+    \param iOffset: read at offset from read index
+    \return type T the value from the read index
+    */
     T get (int iOffset) const
     {
-        try {
-            checkIndex(iOffset);
-        }
-        catch (char const* msg) {
-            std::cout << msg << std::endl;
-//            return;
-        }
-        
         int index = (m_iReadIdx + iOffset) % m_iBuffLength;
+        if (index < 0) {
+            index += m_iBuffLength;
+        }
         float  a = m_ptBuff[index];
         return a;
     }
-    
+
     /*! set buffer content and indices to 0
-     \return void
-     */
+    \return void
+    */
     void reset ()
     {
         memset (m_ptBuff, 0, sizeof(T)*m_iBuffLength);
         m_iReadIdx  = 0;
         m_iWriteIdx = 0;
     }
-    
+
     /*! returns the number of values currently buffered (note: 0 could also mean the buffer is full!)
-     \return int
-     */
+    \return int
+    */
     int getNumValuesInBuffer () const
     {
         return (m_iWriteIdx - m_iReadIdx + m_iBuffLength)%m_iBuffLength;
     }
-    
+
     /*! returns the length of the internal buffer
-     \return int
-     */
+    \return int
+    */
     int getLength () const
     {
         return m_iBuffLength;
@@ -186,18 +181,17 @@ public:
 private:
     CRingBuffer ();
     CRingBuffer(const CRingBuffer& that);
-    
+
     int m_iBuffLength,              //!< length of the internal buffer
-    m_iReadIdx,                 //!< current read index
-    m_iWriteIdx;                //!< current write index
-    
+        m_iReadIdx,                 //!< current read index
+        m_iWriteIdx;                //!< current write index
+
     T   *m_ptBuff;                  //!< data buffer
-    void checkIndex(int newIndex) const{
+    void checkIndex(int newIndex) {
         if (newIndex < 0 || newIndex >= m_iBuffLength ) {
             throw "Exception: index out of range!";
         }
     }
-
     void incIndex(int &index) {
         if (index == m_iBuffLength - 1) {
             index = 0;
@@ -206,7 +200,6 @@ private:
         }
     }
     int status = 0;                 // if status = 0 && read index = write index, put
-    // if status = 1 && read index = write index, read
+                                    // if status = 1 && read index = write index, read
 };
 #endif // __RingBuffer_hdr__
-
